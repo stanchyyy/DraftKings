653376a3509efa698c3893b6d627737f7e37a132
stanchyyy <stanchyyy@gmail.com>
2022-06-07 02:47:41 +0300

    added Token Model

---
+++ Book-lib/Models/Token.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Book_lib.Models
+{
+    public class Token
+    {
+        public string token { get; set; }
+        public string expiresAt { get; set; }
+
+        public Token()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return token.ToString();
+        }
+    }
+}
