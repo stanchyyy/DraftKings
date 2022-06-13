aeeac89752312dd76989433e9cbd58022f4e247c
stanchyyy <stanchyyy@gmail.com>
2022-06-13 03:08:19 +0300

    added iinterface for books and models

---
+++ Test_Book-lib/Models/IBookModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Book_lib.Models
+{
+    public interface IBookModel
+    {
+        AuthorModel Author { get; set; }
+        int Id { get; set; }
+        string Publisher { get; set; }
+        DateTime ReleaseDate { get; set; }
+        string Title { get; set; }
+    }
+}
---
+++ Test_Book-lib/Models/IUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Book_lib.Models
+{
+    public interface IUserModel
+    {
+        string EmailAddress { get; set; }
+        string Password { get; }
+        string UserName { get; set; }
+
+        string ToString();
+    }
+}
